516c0ef551a529a9ae24c4bb3ff404cff2267875
chally <itlab385@gmail.com>
2025-04-21 16:43:42 +0900

    4차

--- api/like.js
+++ api/like.js
@@ -120,11 +120,11 @@ like.get('/user-liked', async (req, res) => {
       .find({ userId })
       .toArray();
 
+      console.log(likedPosts)
     // 해당하는 likedPostId 배열을 리턴
-    const likedPostIds = likedPosts.map(like => like.postId);
-    
+    // const likedPostIds = likedPosts.map(like => like.postId);
     // 클라이언트에 응답
-    res.status(200).json({ likedPostIds });
+    res.status(200).json({ likedPosts });
   } catch (error) {
     console.error("좋아요한 게시물 조회 오류:", error);
     res.status(500).json({ error: "서버 오류" });
